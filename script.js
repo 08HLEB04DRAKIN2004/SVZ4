bb999ef0b7e3ad621f62f9e16c9fc57becbbdac3
04Hleb04 <115426587+04Hleb04@users.noreply.github.com>
2023-11-29 00:04:45 +0300

    fix: update html,css,js

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const API_KEY = 'd3476595417e9eb00d3200fa664d203d';
-const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+const API_KEY = 'd3476595417e9eb00d3200fa664d203d';// ключ авторизации для достпуа к базе данных
+const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;// СТРОКА ЗАПРОСА
 
 const main = document.getElementById('main');
 const search = document.getElementById('search');
 
+const clearSearch = document.getElementById('clear-search');
 
+// Обработчик события для очистки поля ввода
+clearSearch.addEventListener('click', () => {
+    search.value = ''; // Очистить поле ввода
+    main.innerHTML = ''; // Очистить результаты поиска
+    getMovies('venom');
+});
 //запрашивает данные о фильмах с помощью API интерфеййса
 
 async function getMovies(searchTerm) {
-    const res = await fetch(API_URL + searchTerm);
+    const res = await fetch(API_URL + searchTerm);//запрос к серверу
     const data = await res.json();
-
-    showMovies(data.results);
+    if (data.results && data.results.length > 0) {
+        showMovies(data.results);
+    } else {
+        main.innerHTML = '<p>Фильмы не найдены</p>';
+    }
 }
 
 function showMovies(movies) {
@@ -40,9 +50,9 @@ search.addEventListener('keyup', (e) => {
 
         getMovies(searchTerm);
         
-        search.value = '';
-        search.blur();
+        
+ 
     }
 });
 
-getMovies(' ');
+getMovies('venom');
